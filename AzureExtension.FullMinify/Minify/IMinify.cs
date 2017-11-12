7e90e6560d031c5694feba9ef805bdf85e43d12e
Max Melcher <mmelcher@alegri.eu>
2017-11-12 19:53:56 +0100

    Interface

---
+++ AzureExtension.FullMinify/Minify/IMinify.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureExtension.FullMinify.Minify
+{
+    public interface IMinify
+    {
+        void MinifyCSS(string filepath);
+        void MinifyHtml(string filepath);
+        void MinifyJs(string filepath);
+        void Watch();
+        void Minify();
+    }
+}
